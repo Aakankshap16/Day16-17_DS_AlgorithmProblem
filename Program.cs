5e7dd7919099d9da79c36406059e2b6e384c68f2
Aakankshap16 <aakankshap971@gmail.com>
2023-04-07 01:03:13 +0530

    first sort then search in UC9

--- Program.cs
+++ Program.cs
@@ -7,9 +7,19 @@
             Console.WriteLine("Welcome to Data Structures and Algorithms Practice Problem");
             Console.WriteLine("*********************");
 
-            //PRIME NUMBER IN RANGE
-            PrimeNumberRange obj = new PrimeNumberRange();
-            obj.prime();
+            SortingWithGeneric<int> obj = new SortingWithGeneric<int>();
+            int[] arr = new int[7] { 10, 22, 45, 88, 56, 77, 87 };
+            obj.DisplayBubble(arr);
+           int val = obj.BinarySearch(arr, 56);
+            if (val == -1)
+            {
+                Console.WriteLine("Value not found in array.");
+            }
+            else
+            {
+                Console.WriteLine($"Value found at index {val}.");
+            }
+
 
         }
     }
