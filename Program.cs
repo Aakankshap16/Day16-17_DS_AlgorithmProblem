cacf89dab35d1a4bfbfd765eb506d68b01037c8c
Aakankshap16 <aakankshap971@gmail.com>
2023-04-06 23:25:49 +0530

    find prime no in range in UC7

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@
         {
             Console.WriteLine("Welcome to Data Structures and Algorithms Practice Problem");
             Console.WriteLine("*********************");
+
+            //PRIME NUMBER IN RANGE
             PrimeNumberRange obj = new PrimeNumberRange();
             obj.prime();
 
