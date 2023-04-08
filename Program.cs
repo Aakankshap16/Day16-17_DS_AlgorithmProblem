8010f461b4a6a77d198ce9386778801ca824be3d
Aakankshap16 <aakankshap971@gmail.com>
2023-04-08 15:20:37 +0530

    search word from file in uc2

--- Program.cs
+++ Program.cs
@@ -8,22 +8,7 @@
             Console.WriteLine("Welcome to Data Structures and Algorithms Practice Problem");
             Console.WriteLine("*********************");
 
-            List<string> list = new List<string> { "Ram", "Bhole", "Shiv", "Khanha", "Durga", "Lakshmi"};
-            Console.WriteLine("Unsorted List:");
-            foreach (string s in list)
-            {
-                Console.Write(s + " ");
-            }
-            Console.WriteLine();
-
-            List<string> sorted = MergeSortString.Sort(list);
-            Console.WriteLine("Sorted List:");
-            foreach (string s in sorted)
-            {
-                Console.Write(s + " ");
-            }
-            Console.WriteLine();
-
+            SearchFromFile.Check(@"C:\Users\ASHISH PANDEY\source\repos\BinarySearchTree\BinarySearchTree\File.txt");
 
 
         }
