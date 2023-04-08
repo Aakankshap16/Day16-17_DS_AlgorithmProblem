32c95e9bb1167d6e01d62b756df707dc3287d7e6
Aakankshap16 <aakankshap971@gmail.com>
2023-04-08 15:20:18 +0530

    search word from file

---
+++ SearchFromFile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace BinarySearchTree
+{
+    public class SearchFromFile
+    {
+        public static void Check(string path)
+        {
+            Console.WriteLine("Enter Word for search:");
+            string word = Console.ReadLine();
+            string value = File.ReadAllText(path);
+            string[] str = value.Split(" ");
+            foreach (string s in str)
+            {
+                Console.WriteLine($"{s}");
+                if (word == s)
+                {
+                    Console.WriteLine($"Your word found {word}");
+                    return;
+                }
+            }
+            Console.WriteLine($"Your word not found");
+        }
+    }
+}
